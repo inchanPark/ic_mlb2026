eee220cfd2d59ddbbbcf35743e0e7deeeb153516
inchanPark <98707868+inchanPark@users.noreply.github.com>
2025-03-31 01:10:47 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ st.title("📈 투자매력도 평가 툴")
 code = st.text_input("종목 코드를 입력하세요:")
 if code:
     st.success(f"입력한 종목: {code}")
-    st.write("👉 여기에 분석 결과가 나올 예정입니다!")
+    st.write("이 종목의 투자매력도는 **83점**입니다. (샘플 점수)")
